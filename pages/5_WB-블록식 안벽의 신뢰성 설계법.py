030a8746f4906ea0085a43816a00b851df6fbf30
kimchangbo <kcb2002@nate.com>
2026-08-05 15:25:43 +0900

    Update 5_WB-블록식 안벽의 신뢰성 설계법.py

--- pages/5_WB-블록식 안벽의 신뢰성 설계법.py
+++ pages/5_WB-블록식 안벽의 신뢰성 설계법.py
@@ -11,7 +11,7 @@ import base64
 
 with st.sidebar:
     st.markdown("---")
-    st.write("**제작자:** [김창보, 이종태]")
+    st.write("**제작자:** [김창보, 김성호, 남도현, 백서연]")
     st.write("**소속:** [다온기술]")
     st.caption("© 2026 All rights reserved.")
 
